0033dab66b82f1e75f977e58abac05573852cf15
Caroline-Holmstrom <Caroline-Holmstrom@users.noreply.github.com>
2022-02-02 15:18:00 +0100

    Implements attestation report API
    Implements an empty definition of the attestation report API

--- lib/vendors/axis-communications/sv_vendor_axis_communications.c
+++ lib/vendors/axis-communications/sv_vendor_axis_communications.c
@@ -1,20 +1,35 @@
-/**                                                                                                                                                                                                                                    
- * MIT License                                                                                                          
- *                                                                                                                      
- * Copyright (c) 2022 Axis Communications AB                                                                            
- *                                                                                                                      
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and                    
- * associated documentation files (the "Software"), to deal in the Software without restriction,                        
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,                            
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is                        
- * furnished to do so, subject to the following conditions:                                                             
- *                                                                                                                      
- * The above copyright notice and this permission notice (including the next paragraph) shall be                        
- * included in all copies or substantial portions of the Software.                                                      
- *                                                                                                                      
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT                    
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                               
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,                         
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                       
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                              
- */                                 
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Axis Communications AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next paragraph) shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "sv_vendor_axis_communications.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+SignedVideoReturnCode
+sv_vendor_axis_communications_set_attestation_report(__attribute__((unused)) signed_video_t *self,
+    __attribute__((unused)) uint8_t *attestation,
+    __attribute__((unused)) size_t att_size,
+    __attribute__((unused)) uint8_t *cert_chain,
+    __attribute__((unused)) size_t cert_size)
+{
+  return SV_OK;
+}
