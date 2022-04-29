93b37d59fa5dee88c2e7e825aa50578e12150222
Björn Völcker <65532189+bjornvolcker@users.noreply.github.com>
2022-04-29 14:05:51 +0200

    New vendor API get_axis_communications_supplemental_authenticity (#70)
    New vendor API get_axis_communications_supplemental_authenticity
    
    to fetch status of public key used to validate the video.
    
    The API is called before validating the authenticity and is triggered
    if Axis is the manufacturer.
    
    Co-authored-by: bjornvolcker <bjornvolcker@users.noreply.github.com>

--- lib/vendors/axis-communications/sv_vendor_axis_communications.c
+++ lib/vendors/axis-communications/sv_vendor_axis_communications.c
@@ -292,6 +292,29 @@ set_axis_communications_public_key(void *handle,
   return status;
 }
 
+svi_rc
+get_axis_communications_supplemental_authenticity(void *handle,
+    sv_vendor_axis_supplemental_authenticity_t **supplemental_authenticity)
+{
+  if (!handle || !supplemental_authenticity) return SVI_INVALID_PARAMETER;
+
+  sv_vendor_axis_communications_t *self = (sv_vendor_axis_communications_t *)handle;
+
+  // TODO: Fill in the skeleton below step by step.
+  // svi_rc status = SVI_UNKNOWN;
+  // SVI_TRY()
+  //   SVI_THROW(verify_and_parse_certificate_chain(self));
+  //   SVI_THROW(deserialize_attestation(self));
+  //   SVI_THROW(verify_axis_communications_public_key(self));
+
+  // SVI_CATCH()
+  // SVI_DONE(status)
+
+  *supplemental_authenticity = &self->supplemental_authenticity;
+
+  return SVI_OK;
+}
+
 // Definitions of public APIs declared in sv_vendor_axis_communications.h.
 
 SignedVideoReturnCode
