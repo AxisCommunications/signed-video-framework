6d342f6caeb7bf776c7bc1283a5dbc6e569ef3c1
bjornvolcker <bjornvolcker@users.noreply.github.com>
2024-05-07 10:27:07 +0200

    Cleanup of signed_video_helpers.h
    Removes unused includes and updates some descriptions.

--- tests/check/signed_video_helpers.h
+++ tests/check/signed_video_helpers.h
@@ -23,9 +23,9 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>  // size_t
 
 #include "lib/src/includes/signed_video_common.h"  // signed_video_t, SignedVideoCodec
-#include "lib/src/includes/signed_video_openssl.h"  // sign_algo_t
 #include "lib/src/includes/signed_video_sign.h"  // SignedVideoAuthenticityLevel
 #include "lib/src/signed_video_defines.h"  // sv_tlv_tag_t
 #include "test_stream.h"  // test_stream_t
@@ -59,10 +59,10 @@ extern const char *axisDummyCertificateChain;
 extern const int64_t g_testTimestamp;
 
 /* Creates a signed_video_t session and initialize it by setting
- * 1. a path to openssl keys
+ * 1. a private key
  * 2. product info strings
  *
- * new_private_key = Generate a new private key or not.
+ * new_private_key = Generate a new private key, otherwise read from an existing file.
  * This is useful for testing the signing part and generating a signed stream of nalus. */
 signed_video_t *
 get_initialized_signed_video(SignedVideoCodec codec,
@@ -73,26 +73,27 @@ get_initialized_signed_video(SignedVideoCodec codec,
 test_stream_t *
 create_signed_nalus(const char *str, struct sv_setting settings);
 
-/* See function create_signed_nalus_int, with the diffrence that each NALU is split in two parts. */
+/* See function create_signed_nalus_int, with the diffrence that each NAL Unit is split in
+ * two parts. */
 test_stream_t *
 create_signed_splitted_nalus(const char *str, struct sv_setting settings);
 
-/* Creates a test_stream_t with all the NALUs produced after signing. This mimic what leaves the
- * camera.
+/* Creates a test_stream_t with all the NAL Units produced after signing. This mimic what
+ * leaves the camera.
  *
- * The input is a string of characters representing the type of NALUs passed into the signing
- * session.
+ * The input is a string of characters representing the type of NAL Units passed into the
+ * signing session.
  * Example-1: 'IPPIPP' will push two identical GOPs
  *   I-nalu, P-nalu, P-nalu.
  * Example-2: for multi slice, 'IiPpPpIiPpPp' will push two identical GOPs
  *   I-nalu, i-nalu, P-nalu, p-nalu, P-nalu, p-nalu.
  * Valid characters are:
- *   I: I-nalu Indicates first I slice in the current I nalu
- *   i: i-nalu Indicates other than first I slice. Example: second and third slice
- *   P: P-nalu Indicates first P slice in the current P nalu
- *   p: p-nalu Indicates other than first P slice. Example: second and third slice
- *   S: Non signed-video-framework SEI-nalu
- *   X: Invalid nalu, i.e., not a H26x nalu.
+ *   I: I-nalu Indicates first slice in the current I nalu
+ *   i: i-nalu Indicates other than first slice. Example: second and third slice
+ *   P: P-nalu Indicates first slice in the current P nalu
+ *   p: p-nalu Indicates other than first slice. Example: second and third slice
+ *   S: Non signed-video-framework SEI
+ *   X: Invalid nalu, i.e., not a H.26x nalu.
  *
  * settings = the session setup for this test.
  * new_private_key = Generate a new private key or not.
@@ -100,16 +101,16 @@ create_signed_splitted_nalus(const char *str, struct sv_setting settings);
 test_stream_t *
 create_signed_nalus_int(const char *str, struct sv_setting settings, bool new_private_key);
 
-/* Generates a signed video stream of NALUs for a user-owned signed_video_t session.
+/* Generates a signed video stream of NAL Units for a user-owned signed_video_t session.
  *
- * Takes a string of NALU characters ('I', 'i', 'P', 'p', 'S', 'X') as input and generates NALU
- * data for these. Then adds these NALUs to the input session. The generated sei-nalus are added to
- * the stream. */
+ * Takes a string of NAL Unit characters ('I', 'i', 'P', 'p', 'S', 'X') as input and
+ * generates NAL Unit data for these. Then adds these NAL Units to the input session. The
+ * generated sei-nalus are added to the stream. */
 test_stream_t *
 create_signed_nalus_with_sv(signed_video_t *sv, const char *str, bool split_nalus);
 
-/* Removes the NALU list items with position |item_number| from the |list|. The item is, after a
- * check against the expected |type|, then freed. */
+/* Removes the NAL Unit item with position |item_number| from the test stream |list|. The
+ * item is, after a check against the expected |type|, then freed. */
 void
 remove_item_then_check_and_free(test_stream_t *list, int item_number, char type);
 
@@ -118,7 +119,7 @@ remove_item_then_check_and_free(test_stream_t *list, int item_number, char type)
 void
 modify_list_item(test_stream_t *list, int item_number, char type);
 
-/* Checks if a particular TLV tag is present in the NALU. */
+/* Checks if a particular TLV tag is present in the NAL Unit |item|. */
 bool
 tag_is_present(test_stream_item_t *item, SignedVideoCodec codec, sv_tlv_tag_t tag);
 
