0de46c48ce4c294c4fc04b5a32658b3bdf5cf9f0
Yunus Kurt <yunusk@axis.com>
2025-02-17 14:33:58 +0100

    Add functions to convert between timestamps
    Add functions to convert between Unix and 1601-based timestamps
    
    Change-Id: I0baf3c9a4b1d34ecde114fcb7344b4b97785ec21

--- lib/src/sv_common.c
+++ lib/src/sv_common.c
@@ -158,6 +158,20 @@ sign_or_verify_data_free(sign_or_verify_data_t *self)
   free(self);
 }
 
+// Convert Unix timestamp (seconds since 1970) to 1601-based timestamp
+void
+convert_unix_to_1601(int64_t *timestamp)
+{
+  *timestamp = (*timestamp + EPOCH_DIFF) * HUNDRED_NANOSECONDS;
+}
+
+// Convert 1601-based timestamp (100-nanosecond intervals since 1601) to Unix timestamp
+void
+convert_1601_to_unix(int64_t *timestamp)
+{
+  *timestamp = (*timestamp / HUNDRED_NANOSECONDS) - EPOCH_DIFF;
+}
+
 /* Reads the version string and puts the Major.Minor.Patch in the first, second and third element of
  * the array, respectively */
 static bool
