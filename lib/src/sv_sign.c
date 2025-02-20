70c12d17164706ac2ebe33eed1ded5fdd67b4763
bjornvolcker <bjornvolcker@users.noreply.github.com>
2025-02-20 12:43:52 +0100

    Includes missing header

--- lib/src/sv_sign.c
+++ lib/src/sv_sign.c
@@ -30,6 +30,9 @@
 #include "sv_codec_internal.h"  // METADATA_TYPE_USER_PRIVATE
 #include "sv_defines.h"  // svrc_t, sv_tlv_tag_t
 #include "sv_internal.h"  // gop_info_t
+#ifndef HAS_ONVIF
+#include "sv_onvif.h"  // Stubs for ONVIF APIs and structs
+#endif
 #include "sv_openssl_internal.h"
 #include "sv_tlv.h"  // tlv_list_encode_or_get_size()
 
